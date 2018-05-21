ddc13d9582da166a60283f38022b46a962748c3a
James Whitney <jamesdrums@mac.com>
2018-05-21 14:00:13 -0400

    Intial Commit

---
+++ On-the-Map/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  LoginViewController.swift
+//  On-the-Map
+//
+//  Created by James Whitney on 5/21/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
