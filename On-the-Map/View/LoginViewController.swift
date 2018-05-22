90e9000f3a28d898e851b247046115ec3fb4c4f0
James Whitney <jamesdrums@mac.com>
2018-05-22 10:52:30 -0400

    feat: Adds UI elements for user to log in
    The LoginViewController.swift file now contains outlets for
    email and password fields. Adds completeLogin function as an action of
    the LOG IN button. Currently just prints a statement to the console for
    debugging.

--- On-the-Map/View/LoginViewController.swift
+++ On-the-Map/View/LoginViewController.swift
@@ -11,11 +11,19 @@ import Foundation
 
 class LoginViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBAction func completeLogin(_ sender: Any) {
+        print("LOG IN button pressed")
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
