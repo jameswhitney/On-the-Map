6ab097146fcda505d30392e1d6170c0efcc55bb2
James Whitney <jamesdrums@mac.com>
2018-05-25 10:14:06 -0400

    feat: Adds files and classes for Networking tasks

---
+++ On-the-Map/Model/OTMClient.swift
@@ -0,0 +1,12 @@
+//
+//  OTMClient.swift
+//  On-the-Map
+//
+//  Created by James Whitney on 5/25/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+
+class OTMClient: NSObject {
+}
---
+++ On-the-Map/Model/UdacityContants.swift
@@ -0,0 +1,26 @@
+//
+//  UdacityParseConstants.swift
+//  On-the-Map
+//
+//  Created by James Whitney on 5/25/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+
+extension OTMClient {
+    
+    // MARK: Constants
+    struct Constants {
+        
+        // MARK: Parse API Key
+        static let parseApiKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+        
+        // MARK: URLs
+        static let ApiScheme = "https"
+        static let ApiHost = "www.udacity.com"
+        static let ParseApiHost = "parse.udacity.com"
+    }
+    
+    
+}
