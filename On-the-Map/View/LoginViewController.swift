03824af852d90ed6a1a38793e18008f5dcad5c98
James Whitney <jamesdrums@mac.com>
2018-05-30 12:54:28 -0400

    test: Checks if user login works

--- On-the-Map/View/LoginViewController.swift
+++ On-the-Map/View/LoginViewController.swift
@@ -31,7 +31,30 @@ class LoginViewController: UIViewController {
         } else {
             setUIEnabled(false)
             // TODO: create function to authorize and login user
+            userLogin()
         }
+    }
+    
+    private func userLogin() {
+        activityView.startAnimating()
+        let email = emailTextField.text!
+        let password = passwordTextField.text!
+        var request = URLRequest(url: URL(string: "https://www.udacity.com/api/session")!)
+        request.httpMethod = "POST"
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpBody = "{\"udacity\": {\"username\": \"\(email)\", \"password\": \"\(password)\"}}".data(using: .utf8)
+        
+        let session = URLSession.shared
+        let task = session.dataTask(with: request) { data, response, error in
+            if error != nil { // Handle error…
+                return
+            }
+            let range = Range(5..<data!.count)
+            let newData = data?.subdata(in: range) /* subset response data! */
+            print(String(data: newData!, encoding: .utf8)!)
+        }
+        task.resume()
     }
     
     
