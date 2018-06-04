345462efd6a7c17eea6985d3ed0f9b5f0d04f605
James Whitney <jamesdrums@mac.com>
2018-06-03 20:33:57 -0400

    feat: Adds custom classes to view controllers

---
+++ On-the-Map/View/MapViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MapViewController.swift
+//  On-the-Map
+//
+//  Created by James Whitney on 6/3/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - MapViewController: UIViewController
+
+class MapViewController: UIViewController {
+    // TODO:
+}
---
+++ On-the-Map/View/StudentDetailTableViewController.swift
@@ -0,0 +1,16 @@
+//
+//  StudentDetailTableViewController.swift
+//  On-the-Map
+//
+//  Created by James Whitney on 6/3/18.
+//  Copyright © 2018 Udacity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+// MARK: - StudentDetailTableViewController: UITableViewController
+
+class StudentDetailTableViewController: UITableViewController {
+    // TODO:
+}
