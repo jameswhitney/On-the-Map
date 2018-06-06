7e81bf223741d6b929a87a2d3fc238961fa19e26
James Whitney <jamesdrums@mac.com>
2018-06-06 13:42:00 -0400

    feat: Adds Parse constants to model design

--- On-the-Map/Model/OTMContants.swift
+++ On-the-Map/Model/OTMContants.swift
@@ -31,6 +31,34 @@ struct Constants {
         static let SessionExpiration = "expiration"
     }
     
+    struct ParseParameterKeys {
+        // MARK: Parse Parameter Keys
+        static let ApiHeader = "X-Parse-REST-API-Key"
+        static let AppHeader = "X-Parse-Application-Id"
+        
+    }
+    
+    struct ParseParameterValues {
+        // MARK: Parse Parameter Values
+        static let RestApiKey = "QuWThTdiRmTux3YaDseUSEpUKo7aBYM737yKd4gY"
+        static let ParseAppID = "QrX47CA9cyuGewLdsL7o5Eb8iug6Em8ye0dnAbIr"
+        static let ParseLimit = 50
+    }
+    
+    struct ParseResponseKeys {
+        static let Results = "results"
+        static let ObjectID = "objectId"
+        static let UserKey = "uniqueKey"
+        static let FirstName = "firstName"
+        static let LastName = "lastName"
+        static let LocationName = "mapString"
+        static let UserURL = "mediaURL"
+        static let UserLat = "latitude"
+        static let UserLong = "longitude"
+        static let Posted = "createdAt"
+        static let Updated = "updatedAt"
+    }
+    
     
 }
 
